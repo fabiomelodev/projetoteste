a36186b0dfc88668fa30e6f358679198322b3332
Fabio de Melo <fabiomessias98@outlook.com>
2022-06-08 23:39:57 -0300

    Themes

---
+++ wp-bootstrap-starter-child/page-name.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+get_header(); ?>
+
+<section id="primary" class="content-area">
+<div id="main" class="site-main" role="main">
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+<?php endwhile; ?>
+
+</div><!-- #main -->
+</section><!-- #primary -->
+
+<img
+class="img-fluid"
+data-src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/banner-illustration.png"
+alt="Single Temas">
+
+<?php
+
+get_footer();
